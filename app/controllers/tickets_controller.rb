79df48d4b2bd71053fe6067172f1a159364e583c
Iyayi Roland <iyayiroland@gmail.com>
2023-10-03 16:45:36 +0200

    added authorization

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,7 +2,7 @@ class TicketsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @tickets = Ticket.all
+    @tickets = policy_scope(Ticket)
   end
 
   def show
@@ -10,10 +10,12 @@ class TicketsController < ApplicationController
     @response = Response.new
 
     @response = Response.where(ticket_id: @ticket).pluck(:id)
+    authorize @ticket
   end
 
   def new
     @ticket = Ticket.new
+    authorize @ticket
   end
 
   def create
@@ -21,6 +23,7 @@ class TicketsController < ApplicationController
     @reviewer = Reviewer.all.sample
     @ticket.reviewer = @reviewer
     @ticket.user = current_user
+    authorize @ticket
     if @ticket.save!
       redirect_to root_path
     else
@@ -32,6 +35,7 @@ class TicketsController < ApplicationController
     @ticket = Ticket.find(params[:id])
     @ticket.destroy
     redirect_to root_path, status: :see_other
+    authorize @ticket
   end
 
   private
